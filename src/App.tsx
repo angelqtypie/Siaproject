9edcee236198e6a442e8ddb4d8b8523f9bec558c
angelqtypie <angeljustineballaso07@gmail.com>
2025-05-11 01:58:11 +0800

    added3

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
+import { IonApp, IonRouterOutlet} from '@ionic/react';
+import { IonReactHashRouter } from '@ionic/react-router';
 import { Route, Redirect } from 'react-router-dom';
+
 import Home from './pages/Home';
 import AdminLogin from './pages/AdminLogin';
 import AdminDashboard from './pages/admin/adminDashboard';
@@ -13,26 +14,20 @@ import Dashboard from './pages/users/Dashboard';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils */
 import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
 
+
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter basename="/">
+    <IonReactHashRouter>
       <IonRouterOutlet>
         <Route exact path="/home" component={Home} />
         <Route exact path="/adminlogin" component={AdminLogin} />
@@ -41,15 +36,13 @@ const App: React.FC = () => (
         <Route exact path="/viewrequests" component={ViewRequests} />
         <Route exact path="/products" component={AddProducts} />
         <Route exact path="/viewfeedbacks" component={ViewFeedbacks} />
-
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/login" component={Login} />
-
         <Route exact path="/">
           <Redirect to="/home" />
         </Route>
       </IonRouterOutlet>
-    </IonReactRouter>
+    </IonReactHashRouter>
   </IonApp>
 );
 
